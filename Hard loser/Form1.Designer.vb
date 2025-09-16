54710fba089725b8258aff027d703ff090f8438f
justinapostol <justinapostol@SMU-0132>
2025-09-16 08:16:39 +0800

    Add project files.

---
+++ Hard loser/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Monday = New System.Windows.Forms.TextBox()
+        Me.Tuesday = New System.Windows.Forms.TextBox()
+        Me.Wednesday = New System.Windows.Forms.TextBox()
+        Me.Thursday = New System.Windows.Forms.TextBox()
+        Me.Friday = New System.Windows.Forms.TextBox()
+        Me.Saturday = New System.Windows.Forms.TextBox()
+        Me.Sunday = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(331, 111)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Monday"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(331, 140)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Tuesday"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(331, 172)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Wednesday"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(331, 211)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Thursday"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(331, 248)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Friday"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(331, 280)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Saturday"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(331, 319)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(43, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Sunday"
+        '
+        'Monday
+        '
+        Me.Monday.Location = New System.Drawing.Point(422, 111)
+        Me.Monday.Name = "Monday"
+        Me.Monday.Size = New System.Drawing.Size(100, 20)
+        Me.Monday.TabIndex = 7
+        '
+        'Tuesday
+        '
+        Me.Tuesday.Location = New System.Drawing.Point(422, 140)
+        Me.Tuesday.Name = "Tuesday"
+        Me.Tuesday.Size = New System.Drawing.Size(100, 20)
+        Me.Tuesday.TabIndex = 8
+        '
+        'Wednesday
+        '
+        Me.Wednesday.Location = New System.Drawing.Point(422, 172)
+        Me.Wednesday.Name = "Wednesday"
+        Me.Wednesday.Size = New System.Drawing.Size(100, 20)
+        Me.Wednesday.TabIndex = 9
+        '
+        'Thursday
+        '
+        Me.Thursday.Location = New System.Drawing.Point(422, 204)
+        Me.Thursday.Name = "Thursday"
+        Me.Thursday.Size = New System.Drawing.Size(100, 20)
+        Me.Thursday.TabIndex = 10
+        '
+        'Friday
+        '
+        Me.Friday.Location = New System.Drawing.Point(422, 241)
+        Me.Friday.Name = "Friday"
+        Me.Friday.Size = New System.Drawing.Size(100, 20)
+        Me.Friday.TabIndex = 11
+        '
+        'Saturday
+        '
+        Me.Saturday.Location = New System.Drawing.Point(422, 273)
+        Me.Saturday.Name = "Saturday"
+        Me.Saturday.Size = New System.Drawing.Size(100, 20)
+        Me.Saturday.TabIndex = 12
+        '
+        'Sunday
+        '
+        Me.Sunday.Location = New System.Drawing.Point(422, 312)
+        Me.Sunday.Name = "Sunday"
+        Me.Sunday.Size = New System.Drawing.Size(100, 20)
+        Me.Sunday.TabIndex = 13
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(446, 83)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Label8"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(449, 367)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Sunday)
+        Me.Controls.Add(Me.Saturday)
+        Me.Controls.Add(Me.Friday)
+        Me.Controls.Add(Me.Thursday)
+        Me.Controls.Add(Me.Wednesday)
+        Me.Controls.Add(Me.Tuesday)
+        Me.Controls.Add(Me.Monday)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Monday As TextBox
+    Friend WithEvents Tuesday As TextBox
+    Friend WithEvents Wednesday As TextBox
+    Friend WithEvents Thursday As TextBox
+    Friend WithEvents Friday As TextBox
+    Friend WithEvents Saturday As TextBox
+    Friend WithEvents Sunday As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Button1 As Button
+End Class
